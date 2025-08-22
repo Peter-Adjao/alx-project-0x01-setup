49fe2f7745f556716c9a2e7a6eef63d3b1fa35d1
Peter Adjao <pa376499@gmail.com>
2025-08-22 13:57:11 +0000

    fix(users): update Users page to use posts.map for data rendering

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,31 +2,29 @@ import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
 interface UsersPageProps {
-    users: UserProps[]; // Returned array of users
+  posts: UserProps[];
 }
 
-const Users: React.FC<UsersPageProps> = ({ users }) => { 
-    return (
-        <div className="container mx-auto p-6">
-            <h1 className="tet-3xl font-bold mb-6 text-center">Users</h1>
-            {users.map((user)  => (
-                <UserCard key={user.id} {...user}/>
-
-            ))}
-        </div>
-    );
-
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6 text-center">Users</h1>
+      {posts.map((user) => (
+        <UserCard key={user.id} {...user} />
+      ))}
+    </div>
+  );
 };
 
 export async function getStaticProps() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
 
-    return {
-        props: {
-            users,
-        },
-    };
+  return {
+    props: {
+      posts,
+    },
+  };
 }
 
 export default Users;
